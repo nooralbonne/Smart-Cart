4cd625fe7dd8706db2f7f23642e910c002f0cd51
nooralbonne <24042306@student.ltuc.com>
2024-07-15 14:25:36 +0300

    add#

--- ShoppingCartApplication/ShoppingCart.cs
+++ ShoppingCartApplication/ShoppingCart.cs
@@ -1,56 +1,43 @@
-﻿using System;
+﻿// ShoppingCart.cs
+using ShoppingCartApplication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ShoppingCartApplication
+public class ShoppingCart
 {
-    public class ShoppingCart
+    private List<Product> items;
+
+    public ShoppingCart()
     {
-        private List<Product> items;
+        items = new List<Product>();
+    }
 
-        public ShoppingCart()
-        {
-            items = new List<Product>();
-        }
+    public void AddItem(Product product)
+    {
+        items.Add(product);
+    }
 
-        public void AddItem(Product product)
+    public void RemoveItem(string productName)
+    {
+        var product = items.FirstOrDefault(p => p.Name == productName);
+        if (product != null)
         {
-            items.Add(product);
+            items.Remove(product);
         }
+        else
+        {
+            Console.WriteLine("Product not found in the cart.");
+        }
+    }
 
-        public void RemoveItem(string productName)
-        {
-            var product = items.FirstOrDefault(p => p.Name == productName);
-            if (product != null)
-            {
-                items.Remove(product);
-            }
-            else
-            {
-                Console.WriteLine("Product not found in the cart.");
-            }
-        }
+    public List<Product> ViewCart()
+    {
+        return items;
+    }
 
-        public void ViewCart()
-        {
-            if (items.Count == 0)
-            {
-                Console.WriteLine("Your cart is empty.");
-            }
-            else
-            {
-                foreach (var item in items)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-        }
-
-        public decimal CalculateTotalCost()
-        {
-            return items.Sum(item => item.Price);
-        }
+    public decimal CalculateTotalCost()
+    {
+        return items.Sum(item => item.Price);
     }
 }
